0c5b748acadc41ce995759433c2c7d6b843c7495
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-04-10 09:29:51 +0300

    #4 Estonian translations

--- ts_setup/comms-portal/src/intl/en.ts
+++ ts_setup/comms-portal/src/intl/en.ts
@@ -4,5 +4,9 @@ export const en = {
   'gamut.locale.en': 'English',
   'gamut.buttons.serviceSelect': 'Select topic',
   'gamut.buttons.search': 'Search',
-  'gamut.search.popover.title': 'Search'
+  'gamut.search.popover.title': 'Search',
+  'gamut.search.popover.input.placeholder': 'Search',
+  'gamut.search.results.allResults': 'All results',
+  'gamut.search.results.noResults': 'No results',
+  'gamut.noValueIndicatorColon': ':'
 }
