29c5b545f99bf24d3e242eb9ba29eb5e2b4a5a2a
Olev Mutso <olev.mutso@resys.io>
2025-10-13 15:53:05 +0300

    theming

---
+++ ts_setup/on4kst-chat/api-secure-storage/SecureStorage.ts
@@ -0,0 +1,95 @@
+import * as EncryptedStorage from 'expo-secure-store';
+import React from 'react';
+
+
+export interface SecureStorage {
+  saveCredentials(callsign: string, password: string, locator: string | null, calibrationOffset: number | null): Promise<void>;
+  saveToken(token: string): Promise<void>;
+  getCallsign(): Promise<string | null>;
+  getPassword(): Promise<string | null>;
+  getToken(): Promise<string | null>;
+
+  getMyLocaltion(): Promise<{ locator: string | null, calibrationOffset: number | null }>;
+  
+  clearAll(): Promise<void>;
+}
+
+
+class SecureStorageImpl implements SecureStorage {
+
+  private readonly KEYS = {
+    CALLSIGN: 'user_callsign',
+    PASSWORD: 'user_password',
+    TOKEN: 'user_token',
+    LOCATOR: 'user_locator',
+    ROTATOR_OFFSET: 'user_offset',
+  };
+
+  async saveCredentials(callsign: string, password: string, locator: string | null, calibrationOffset: number | null): Promise<void> {
+    await EncryptedStorage.setItemAsync(this.KEYS.CALLSIGN, callsign);
+    await EncryptedStorage.setItemAsync(this.KEYS.PASSWORD, password);
+
+    if(locator) {
+      await EncryptedStorage.setItemAsync(this.KEYS.LOCATOR, locator);
+    }
+    if(calibrationOffset) {
+      await EncryptedStorage.setItemAsync(this.KEYS.ROTATOR_OFFSET, calibrationOffset + '');
+    }
+  }
+
+  async saveToken(token: string): Promise<void> {
+    await EncryptedStorage.setItemAsync(this.KEYS.TOKEN, token);
+  }
+
+  async getCallsign(): Promise<string | null> {
+    return await EncryptedStorage.getItem(this.KEYS.CALLSIGN);
+  }
+
+  async getPassword(): Promise<string | null> {
+    return await EncryptedStorage.getItem(this.KEYS.PASSWORD);
+  }
+
+  async getToken(): Promise<string | null> {
+    return await EncryptedStorage.getItem(this.KEYS.TOKEN);
+  }
+
+  async getRotatorOffset(): Promise<string | null> {
+    return await EncryptedStorage.getItem(this.KEYS.ROTATOR_OFFSET);
+  }
+
+  async getLocator(): Promise<string | null> {
+    return await EncryptedStorage.getItem(this.KEYS.LOCATOR);
+  }
+
+  async getMyLocaltion(): Promise<{ locator: string | null; calibrationOffset: number | null; }> {
+    const offset = EncryptedStorage.getItem(this.KEYS.ROTATOR_OFFSET);
+    return {
+      locator: EncryptedStorage.getItem(this.KEYS.LOCATOR),
+      calibrationOffset: offset ? Number.parseInt(offset) : null
+    };
+  }
+
+  async clearAll(): Promise<void> {
+    await EncryptedStorage.deleteItemAsync(this.KEYS.CALLSIGN);
+    await EncryptedStorage.deleteItemAsync(this.KEYS.PASSWORD);
+    await EncryptedStorage.deleteItemAsync(this.KEYS.TOKEN);
+    await EncryptedStorage.deleteItemAsync(this.KEYS.LOCATOR);
+    await EncryptedStorage.deleteItemAsync(this.KEYS.ROTATOR_OFFSET);
+  }
+}
+
+export const secureStorage: SecureStorage = new SecureStorageImpl();
+
+
+export const useSecureStorage = () => {
+
+  const getToken = React.useCallback(() => Promise.all(
+    [ secureStorage.getCallsign(), 
+      secureStorage.getPassword(), 
+      secureStorage.getMyLocaltion(),
+      secureStorage.getToken()])
+      .then(([callsign, password, me, raw]) => ({ callsign, password, me, raw}))
+  , [secureStorage]);
+
+  return { secureStorage, getToken };
+};
