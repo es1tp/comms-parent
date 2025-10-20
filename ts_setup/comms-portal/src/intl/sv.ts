b40d814c62ec2edad17d2c03266e437182c7c2d0
Olev Mutso <olev.mutso@resys.io>
2025-10-20 18:11:14 +0300

    locale ctx

---
+++ ts_setup/comms-portal/src/intl/sv.ts
@@ -0,0 +1,11 @@
+export const sv = {
+  'week': 'Vecka',
+  'work_week': 'Arbetsvecka',
+  'day': 'Dag',
+  'month': 'Månad',
+  'previous': 'Föregående',
+  'next': 'Nästa',
+  'today': 'Idag',
+  'agenda': 'Agenda',
+  'showMore': 'till'
+}
